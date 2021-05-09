baf36c99d28f5b9550a569bb228a02eb575eb184
Ali Caglayan <alizter@gmail.com>
2021-05-09 23:32:18 +0200

    construction of localization mostly finished

---
+++ theories/RingsExtra.v
@@ -0,0 +1,26 @@
+From HoTT Require Import Basics Types Algebra.Rings.
+
+(** * Extra lemmas about rings *)
+
+(** These should probably go in the main library. *)
+
+Section ExtraLemmas.
+
+  Context {R : CRing} (x y z : R).
+
+  Lemma rng_mult_move_left_assoc : x * y * z = y * x * z.
+  Proof.
+    f_ap; apply rng_mult_comm.
+  Defined.
+
+  Lemma rng_mult_move_right_assoc : x * (y * z) = y * (x * z).
+  Proof.
+    refine (rng_mult_assoc _ _ _ @ _ @ (rng_mult_assoc _ _ _)^).
+    apply rng_mult_move_left_assoc.
+  Defined.
+
+  Definition rng_negate_plus : - (x + y) = - x - y := negate_plus_distr _ _.
+
+  Definition rng_negate_zero : - 0 = 0 :> R := negate_0.
+
+End ExtraLemmas.
